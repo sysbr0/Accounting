149deb8b1879888abe85ddc23e2bb52a12b50792
MOHAMMED ALSBR <112288155+sysbr0@users.noreply.github.com>
2024-07-10 15:58:04 +0300

    allow

--- accounting/accounting/settings.py
+++ accounting/accounting/settings.py
@@ -10,7 +10,7 @@ DEBUG = False
 
 # settings.py
 
-ALLOWED_HOSTS = ['accounting-v5va.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
