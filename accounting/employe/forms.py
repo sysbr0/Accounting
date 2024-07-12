31ff39e39b935877b32c488cddb708e52e126bf0
MOHAMMED ALSBR <112288155+sysbr0@users.noreply.github.com>
2024-07-12 21:03:50 +0300

    Update forms.py

--- accounting/employe/forms.py
+++ accounting/employe/forms.py
@@ -2,10 +2,6 @@
 from django import forms
 from .models import Employee
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-
-
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
